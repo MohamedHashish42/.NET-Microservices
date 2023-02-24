ad3b012bba426944d6c20a939acbd7ca1c3ba9b4
MohamedHashish42 <mohamed.hashish42@gmail.com>
2023-02-24 11:26:01 +0200

    Building The First Service (Data Layer - Repository)

--- Project/PlatformService/Startup.cs
+++ Project/PlatformService/Startup.cs
@@ -23,6 +23,7 @@ namespace PlatformService
         {
             services.AddDbContext<AppDbContext>(opt => opt.
                                                 UseInMemoryDatabase("InMem"));
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
