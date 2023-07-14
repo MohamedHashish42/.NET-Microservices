9cc558b7321e1b41607b71724203d9109a0c0a19
MohamedHashish42 <mohamed.hashish42@gmail.com>
2023-07-14 20:50:42 +0300

    Starting With SQL Server (Updating our Platform Service to use SQL Server (Adding Production Settings))

--- Project/PlatformService/Startup.cs
+++ Project/PlatformService/Startup.cs
@@ -13,21 +13,31 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt => opt.
-                                             UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            if (_env.IsProduction())
+            {
+                Console.WriteLine("--> Using SqlServer Db");
+                services.AddDbContext<AppDbContext>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            }
+            else
+            {
+                Console.WriteLine("--> Using InMem Db");
+                services.AddDbContext<AppDbContext>(opt => opt.
+                                                    UseInMemoryDatabase("InMem"));
+            }
 
-            // services.AddDbContext<AppDbContext>(opt => opt.
-            //                                     UseInMemoryDatabase("InMem"));
             services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddHttpClient<IHttpCommandDataClient, HttpCommandDataClient>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
