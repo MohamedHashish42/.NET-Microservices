4ea5b33fab9b1e61a0fee3e25b4088d2d7e8c07d
MohamedHashish42 <mohamed.hashish42@gmail.com>
2023-10-22 22:19:41 +0300

    Multi-Resource API (Controller & Actions)

--- Project/CommandsService/Controllers/PlatformsController.cs
+++ Project/CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -7,17 +11,31 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
+            _repository = repository;
+            _mapper = mapper;
         }
 
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandsService");
+
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
 
         [HttpPost]
         public ActionResult TestConnection()
         {
-            Console.WriteLine("--> Successful Connection");
+           Console.WriteLine("--> Successful Connection");
 
-            return Ok();
+            return Ok("--> Successful Connection");
         }
     }
 }
